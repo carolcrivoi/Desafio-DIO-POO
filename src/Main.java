84d44c92ba3044724f3beb6c03fb10ca8d4e9990
Ana <ac.crivoi@gmail.com>
2022-12-22 22:23:27 -0300

    final

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import br.com.dio.desafio.Dominio.Curso;
+import br.com.dio.desafio.Dominio.Mentoria;
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição Curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Js");
+        curso2.setDescricao("Descrição Curso Js");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Descrição Mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+    }
+}
